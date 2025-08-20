1e2a939606402429c4a87e794bf7081b2c7cc79d
Erick01-ferreira <erickferreiraever@gmail.com>
2025-08-20 17:46:34 -0300

    Adicionado reparos

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,16 @@
-import { Route, Routes } from "react-router-dom";
-import Home from "./pages/home/Home";
-import Relatorios from "./pages/relatorios/Relatorios";
-import Login from "./pages/login/Login";
-import Ocorrencias from "./pages/ocorrencias/Ocorrencias";
-import Usuarios from "./pages/usuarios/Usuarios";
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Home from './pages/home/Home';
+import Login from './pages/login/Login';
 
-export default function App() {
+function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
-      <Route path="/relatorios" element={<Relatorios />} />
       <Route path="/login" element={<Login />} />
-      <Route path="/ocorrencias" element={<Ocorrencias />} />
-      <Route path="/usuarios" element={<Usuarios />} />
+      {/* você pode adicionar outras rotas aqui */}
     </Routes>
   );
 }
+
+export default App; // ✅ aqui é o lugar certo
--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import { BrowserRouter } from 'react-router-dom';
 import { ChakraProvider } from '@chakra-ui/react';
 import { AuthProvider } from './context/AuthContext';
+import App from './App'; // ✅ só importar
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <ChakraProvider>
-      <AuthProvider>
-        <App />
-      </AuthProvider>
-    </ChakraProvider>
+    <BrowserRouter>
+      <ChakraProvider>
+        <AuthProvider>
+          <App />
+        </AuthProvider>
+      </ChakraProvider>
+    </BrowserRouter>
   </React.StrictMode>
 );
